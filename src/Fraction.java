8d2c93fddc4945d252a213639f7417d0b4dc2b1c
Maëlo SANCHEZ-GUENRO <maelo.sanchezguenro@ens.uvsq.fr>
2020-09-28 16:34:50 +0200

    Construction de Fraction
    Ajout d'un corps à la classe fraction et d'un test de cette classe dans Main

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,23 @@
 
 public class Fraction {
+	
+	private int dividende;
+	private int diviseur;
+	private int result; 
+	
+	
+	public Fraction(int dividende, int diviseur) {
+		this.dividende = dividende;
+		this.diviseur = diviseur;
+		result = dividende/diviseur;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Fraction [dividende=" + dividende + ", diviseur=" + diviseur + ", result=" + result + "]";
+	}
+	
+	
 
 }
