45930795cb74a427c1718f703d28467d00aad18b
Maëlo SANCHEZ-GUENRO <maelo.sanchez-guenro@ens.uvsq.fr>
2020-10-03 16:31:25 +0200

    Crétaion et test du corps de la classe Fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,21 @@
 
 public class Fraction {
+	
+	private int dividende;
+	private int diviseur;
+	private double result; 
+	
+	
+	public Fraction(int dividende, int diviseur) {
+		this.dividende = dividende;
+		this.diviseur = diviseur;
+		result = dividende/(double)diviseur;
+	}
 
+
+	@Override
+	public String toString() {
+		return "Fraction [dividende=" + dividende + ", diviseur=" + diviseur + ", result=" + result + "]";
+	}
+	
 }
